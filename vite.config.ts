1ad65942144e037dacf58b6d5c448ba011199b04
loctt2 <loctt2@vng.com.vn>
2025-10-04 15:30:12 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        // Tắt Rolldown hoàn toàn, ép Vite quay lại Rollup
+        rollupOptions: {},
+
+        // (Tuỳ chọn) tối ưu build output
+        // outDir: 'dist',
+        sourcemap: false,
+    },
 })
